8d01931579a4534157a4b44e78b09a92e14d2133
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-03-29 12:16:26 +0100

    ./create_data_image.sh: Do not require root

--- create_data_image.sh
+++ create_data_image.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Currently everything runs as root, but that is going to change.
-USER_UID=0
-USER_GID=0
-
-source ./su_command.sh
-
 TOTAL_SIZE=0
 
 echo "Gathering applications..."
-APPS=
-for app in apps/*
-do
-	if test -f $app
-	then
-		APPS="${APPS} $app"
-		SIZE=$(stat -Lc %s ${app})
+for app in apps/*; do
+	if test -f "$app";	then
+		SIZE="$(stat -Lc %s "$app")"
 		TOTAL_SIZE=$((${TOTAL_SIZE} + ${SIZE}))
 		echo "app: ${app} - $((${SIZE} / 1024)) kB"
-	elif [ $app != "apps/*" ]
-	then
+	else
 		echo "skipping non-regular file: $app"
 	fi
 done
@@ -30,27 +19,27 @@ echo
 # larger so the image stays small.
 IMAGE_SIZE=$((8 + ${TOTAL_SIZE} / (920*1024)))
 
+set -e
+echo "Preparing data for the data partition..."
+rm -rf tmp-data
+mkdir tmp-data
+install -m 755 -d tmp-data/apps
+for app in apps/*; do
+	if test -f "$app"; then
+		install -m 644 -t tmp-data/apps/ "$app"
+	fi
+done
+install -m 755 -d tmp-data/local/etc/init.d
+install -m 755 resize_data_part_launcher.target-sh tmp-data/local/etc/init.d/S00resize
+install -m 755 resize_data_part.target-sh tmp-data/resize_data_part.sh
+
 echo "Creating data partition of ${IMAGE_SIZE} MB..."
 mkdir -p images
 dd if=/dev/zero of=images/data.bin bs=1M count=${IMAGE_SIZE}
 MKE2FS_CONFIG=mke2fs.conf /sbin/mkfs.ext4 -b4096 -I128 -m3 \
-		-E lazy_itable_init=0,lazy_journal_init=0,resize=268435456 \
+		-E lazy_itable_init=0,lazy_journal_init=0,resize=268435456,root_owner=0:0 \
+		-d tmp-data \
 		-F images/data.bin
 echo
 
-echo "Populating data partition..."
-echo "(this step needs superuser privileges)"
-mkdir mnt
-${SU_CMD} "
-	mount images/data.bin mnt -o loop &&
-	install -m 755 -o ${USER_UID} -g ${USER_GID} -d mnt/apps/ &&
-	if [ \"${APPS}\" != \"\" ]
-	then
-		install -m 644 -o ${USER_UID} -g ${USER_GID} -t mnt/apps/ ${APPS}
-	fi &&
-	install -m 755 -o 0 -g 0 -d mnt/local/etc/init.d &&
-	install -m 755 -o 0 -g 0 resize_data_part_launcher.target-sh mnt/local/etc/init.d/S00resize &&
-	install -m 755 -o 0 -g 0 resize_data_part.target-sh mnt/resize_data_part.sh &&
-	umount mnt
-	"
-rmdir mnt
+rm -rf tmp-data
